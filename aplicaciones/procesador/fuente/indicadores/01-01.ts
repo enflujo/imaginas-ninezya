1d525a6de8fbfae3e86e35c30b6c1b8350b97dc4
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-09-01 13:01:57 -0500

    ✨ Procesar departamento y municipio

--- aplicaciones/procesador/fuente/indicadores/01-01.ts
+++ aplicaciones/procesador/fuente/indicadores/01-01.ts
@@ -1,5 +1,5 @@
-import { limpiarMunicipio } from '@/limpieza/lugar';
-import type { Errata, Municipio } from '@/tipos';
+import { limpiarDepartamento, limpiarMunicipio } from '@/limpieza/lugar';
+import type { Departamento, Errata, Municipio } from '@/tipos';
 import { guardarJSON } from '@/utilidades/ayudas';
 import maquinaXlsx from '@/utilidades/maquinaXlsx';
 
@@ -9,35 +9,69 @@ export type VariablesYa1_1 = {
   tacued: number;
 };
 
-type Cobertura = {
-  [año: string]: [departamento: string, porcentaje: number][];
+type Respuesta = {
+  [año: string]: [municipio: string, porcentaje: number][];
 };
 const errata: { fila: number; error: string }[] = [];
 
 export default async () => {
-  const respuesta: Cobertura = {};
+  const datosMunicipios: Respuesta = {};
+  const datosDepartamentos: Respuesta = {};
+
   await maquinaXlsx('1.1: salud - seguridad alimentaria', 'YA1_1.1', 'Sheet1', procesador);
-  guardarJSON(respuesta, 'ya1-1-mun');
+  guardarJSON(datosMunicipios, 'ya1-1-mun');
+
+  for (const año in datosMunicipios) {
+    const datosAño = datosMunicipios[año];
+    const deps: { [codDep: string]: number[] } = {};
+
+    datosAño.forEach((municipio) => {
+      const codDepartamento = municipio[0].slice(0, 2);
+
+      if (!deps[codDepartamento]) {
+        deps[codDepartamento] = [];
+      }
+
+      deps[codDepartamento].push(municipio[1]);
+    });
+
+    for (const codDep in deps) {
+      const departamento = limpiarDepartamento(+codDep);
+
+      if (departamento.hasOwnProperty('error')) {
+        errata.push({ fila: Infinity, error: (departamento as Errata).mensaje });
+      } else {
+        if (!datosDepartamentos[año]) {
+          datosDepartamentos[año] = [];
+        }
+        const suma = deps[codDep].reduce((valorAnterior, valorActual) => valorAnterior + valorActual, 0);
+        const porcentaje = suma / deps[codDep].length;
+        datosDepartamentos[año].push([(departamento as Departamento)[0], porcentaje]);
+      }
+    }
+  }
+
+  guardarJSON(datosDepartamentos, 'ya1-1-dep');
+
   guardarJSON(errata, 'Errata YA1.1_mun');
 
   function procesador(fila: VariablesYa1_1, numeroFila: number) {
-    const datos = limpiarMunicipio(fila.codmpio);
+    const municipio = limpiarMunicipio(fila.codmpio);
 
-    if (datos.hasOwnProperty('error')) {
-      errata.push({ fila: numeroFila, error: (datos as Errata).mensaje });
+    if (municipio.hasOwnProperty('error')) {
+      errata.push({ fila: numeroFila, error: (municipio as Errata).mensaje });
       return;
     }
 
+    /** Procesar Municipio */
     const año = fila.ano;
 
-    if (!respuesta[año]) {
-      respuesta[año] = [];
+    if (!datosMunicipios[año]) {
+      datosMunicipios[año] = [];
     }
 
     if (fila.tacued) {
-      respuesta[año].push([(datos as Municipio)[3], fila.tacued]);
+      datosMunicipios[año].push([(municipio as Municipio)[3], fila.tacued]);
     }
-
-    // console.log(departamentoLimpio);
   }
 };
