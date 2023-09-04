b35920c750fe509353cda064c458f59809d686c5
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-09-04 08:25:14 -0500

    👷 Avances

--- aplicaciones/procesador/fuente/indicadores/03-01.ts
+++ aplicaciones/procesador/fuente/indicadores/03-01.ts
@@ -1,76 +1,73 @@
 // Tasa de Cobertura Bruta
 
-import { limpiarDepartamento } from '@/limpieza/lugar';
-import type { Departamento, Errata } from '@/tipos';
+import { limpiarDepartamento, limpiarMunicipio } from '@/limpieza/lugar';
+import type { Departamento, Errata, Municipio, RespuestaPorcentaje } from '@/tipos';
 import { guardarJSON } from '@/utilidades/ayudas';
 import maquinaXlsx from '@/utilidades/maquinaXlsx';
 
 export type VariablesSaberPlano = {
-  AÑO: number;
-  CÓDIGO_DEPARTAMENTO: number;
-  DEPARTAMENTO: string;
-  POBLACIÓN_5_16: number;
-  TASA_MATRICULACIÓN_5_16: number;
-  COBERTURA_NETA: number;
-  COBERTURA_NETA_TRANSICIÓN: number;
-  COBERTURA_NETA_PRIMARIA: number;
-  COBERTURA_NETA_SECUNDARIA: number;
-  COBERTURA_NETA_MEDIA: number;
-  COBERTURA_BRUTA: number;
-  COBERTURA_BRUTA_TRANSICIÓN: number;
-  COBERTURA_BRUTA_PRIMARIA: number;
-  COBERTURA_BRUTA_SECUNDARIA: number;
-  COBERTURA_BRUTA_MEDIA: number;
-  DESERCIÓN: number;
-  DESERCIÓN_TRANSICIÓN: number;
-  DESERCIÓN_PRIMARIA: number;
-  DESERCIÓN_SECUNDARIA: number;
-  DESERCIÓN_MEDIA: number;
-  APROBACIÓN: number;
-  APROBACIÓN_TRANSICIÓN: number;
-  APROBACIÓN_PRIMARIA: number;
-  APROBACIÓN_SECUNDARIA: number;
-  APROBACIÓN_MEDIA: number;
-  REPROBACIÓN: number;
-  REPROBACIÓN_TRANSICIÓN: number;
-  REPROBACIÓN_PRIMARIA: number;
-  REPROBACIÓN_SECUNDARIA: number;
-  REPROBACIÓN_MEDIA: number;
-  REPITENCIA: number;
-  REPITENCIA_TRANSICIÓN: number;
-  REPITENCIA_PRIMARIA: number;
-  REPITENCIA_SECUNDARIA: number;
-  REPITENCIA_MEDIA: number;
-};
-
-type Cobertura = {
-  [año: string]: [departamento: string, porcentaje: number][];
+  ano: number;
+  codmpio: number;
+  cobertura_bruta: number;
 };
 
 const errata: { fila: number; error: string }[] = [];
 
 export default async () => {
-  const respuesta: Cobertura = {};
-  await maquinaXlsx('3.1: Edu - Cobertura', 'Estadisticas_Men', 'Sheet 1', procesador);
-  guardarJSON(respuesta, 'ya3-1-dep');
-  guardarJSON(errata, 'Errata YA3.1_dep');
+  const datosMunicipios: RespuestaPorcentaje = {};
+  const datosDepartamentos: RespuestaPorcentaje = {};
+  await maquinaXlsx('3.1: Edu - Cobertura', 'YA3_3.1', 'Sheet 1', procesador);
+  guardarJSON(datosMunicipios, 'ya3-1-mun');
+
+  for (const año in datosMunicipios) {
+    const datosAño = datosMunicipios[año];
+    const deps: { [codDep: string]: number[] } = {};
+
+    datosAño.forEach((municipio) => {
+      const codDepartamento = municipio[0].slice(0, 2);
+
+      if (!deps[codDepartamento]) {
+        deps[codDepartamento] = [];
+      }
+
+      deps[codDepartamento].push(municipio[1]);
+    });
+
+    for (const codDep in deps) {
+      const departamento = limpiarDepartamento(+codDep);
+
+      if (departamento.hasOwnProperty('error')) {
+        errata.push({ fila: Infinity, error: (departamento as Errata).mensaje });
+      } else {
+        if (!datosDepartamentos[año]) {
+          datosDepartamentos[año] = [];
+        }
+        const suma = deps[codDep].reduce((valorAnterior, valorActual) => valorAnterior + valorActual, 0);
+        const porcentaje = suma / deps[codDep].length;
+        datosDepartamentos[año].push([(departamento as Departamento)[0], porcentaje]);
+      }
+    }
+  }
+
+  guardarJSON(datosDepartamentos, 'ya3-1-dep');
+  guardarJSON(errata, 'Errata YA3.1');
 
   function procesador(fila: VariablesSaberPlano, numeroFila: number) {
-    const datos = limpiarDepartamento(fila.CÓDIGO_DEPARTAMENTO);
+    const datos = limpiarMunicipio(fila.codmpio);
 
     if (datos.hasOwnProperty('error')) {
       errata.push({ fila: numeroFila, error: (datos as Errata).mensaje });
       return;
     }
 
-    const año = fila.AÑO;
+    const año = fila.ano;
 
-    if (!respuesta[año]) {
-      respuesta[año] = [];
+    if (!datosMunicipios[año]) {
+      datosMunicipios[año] = [];
     }
 
-    if (fila.COBERTURA_BRUTA) {
-      respuesta[año].push([(datos as Departamento)[0], fila.COBERTURA_BRUTA]);
+    if (fila.cobertura_bruta) {
+      datosMunicipios[año].push([(datos as Municipio)[0], fila.cobertura_bruta]);
     }
   }
 };
