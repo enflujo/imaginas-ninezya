fe7138ce71d35aeb240d439c27a654c34428cb1b
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2023-10-10 14:04:32 -0500

    ✨ Procesar ya1-2

--- aplicaciones/procesador/fuente/aplicacion.ts
+++ aplicaciones/procesador/fuente/aplicacion.ts
@@ -1,4 +1,5 @@
 import ya1_1 from '@/indicadores/01-01';
+import ya1_2 from '@/indicadores/01-02';
 import ya1_4 from '@/indicadores/01-04';
 import ya1_6 from '@/indicadores/01-06';
 import ya2_5 from '@/indicadores/02-05';
@@ -6,6 +7,7 @@ import ya3_1 from '@/indicadores/03-01';
 
 async function inicio() {
   await ya1_1();
+  await ya1_2();
   // await ya1_4();
   // await ya1_6();
   // await ya2_5();
