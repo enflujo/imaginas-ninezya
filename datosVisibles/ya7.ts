ef2ded646e396d0bc37d85d96940282933f7c347
anattolia <anattolia@gmail.com>
2024-10-18 13:56:24 -0500

    🐛 mostrar info cuando no hay datos

--- datosVisibles/ya7.ts
+++ datosVisibles/ya7.ts
@@ -6,7 +6,7 @@ export const ya7: DatosYa = {
   ruta: 'fortalecimiento-familiar',
   definicion:
     'Es crucial fortalecer a las familias para proteger y cuidar a la niñez, asegurando vínculos emocionales y cumpliendo normativas como la Declaración Universal de Derechos Humanos. La asistencia a las familias, respaldada por la Convención sobre los Derechos del Niño y la Constitución Nacional, es esencial para la crianza. El Plan Nacional de Desarrollo (2018-2022) destaca la importancia de las familias como garantes de la protección de los niños, requiriendo oportunidades y capacidades para enfrentar su entorno y asegurar el bienestar de todos sus miembros. La familia desempeña un papel crucial en la socialización y protección integral de la niñez.',
-  hay_datos: false,
+  hay_datos: true,
   indicadores: [
     {
       nombre: 'Fortalecimiento Familiar del cuidado y la crianza de la niñez',
