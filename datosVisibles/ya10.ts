3580ac52997c78cd96dc5089f96c46936109b7ce
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-11-14 10:53:10 -0500

    📝 Nuevo texto YA10

--- datosVisibles/ya10.ts
+++ datosVisibles/ya10.ts
@@ -4,7 +4,7 @@ export const ya10: DatosYa = {
   nombre: 'Cultura de paz, reconciliación y convivencia',
   imagen: 'ya10',
   ruta: 'paz-reconciliacion',
-  definicion: `Es crucial que los menores en Colombia participen en la construcción de una cultura de paz tras el conflicto armado. Reconociendo sus experiencias y dándoles voz activa en el diseño de acciones para la reconciliación, la niñez desempeña un papel esencial. Los gobiernos locales, especialmente en los 170 municipios afectados por el conflicto, deben proporcionarles espacio, priorizando la restitución de derechos y el interés superior de la niñez según normativas nacionales e internacionales.`,
+  definicion: `Es crucial que los menores de edad en Colombia participen en la construcción de una cultura de paz, reconociendo sus experiencias y dándoles voz activa en el diseño de acciones para la reconciliación. Los gobiernos locales, especialmente en los 170 municipios afectados por el conflicto, deben proporcionarles espacio, priorizando la restitución de derechos y el interés superior de la niñez, según normativas nacionales e internacionales.`,
   hay_datos: true,
   indicadores: [
     {
